195814be440fa7811bfef2f42c856b86d2b89e1e
Nicola Galgano <optimus4joomla@gmail.com>
2020-02-04 00:21:29 +0100

    fix namespace (#1)

--- component/backend/View/Cpanel/HtmlView.php
+++ component/backend/View/Cpanel/HtmlView.php
@@ -6,7 +6,7 @@
  * @version 1.0
  */
 
-namespace Wilsonge\Component\Overload\Administrator\View\Cpanels;
+namespace Wilsonge\Component\Overload\Administrator\View\Cpanel;
 
 defined('_JEXEC') or die();
 
